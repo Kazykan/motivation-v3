d40e7fe144ecdcf0b672b35003c0d6df43057ba0
Kazykan <rufat@kocherbaev.ru>
2025-01-10 09:38:30 +0300

    add header

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Header } from "@/components/header"
 import { ModeToggle } from "@/components/mode-toggle"
 import { ToggleGroupCalendar } from "@/components/toggle-calendar"
 import { Button } from "@/components/ui/button"
@@ -11,22 +12,22 @@ import {
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main>
-        <Button>Hi</Button>
-        <ModeToggle />
-        <Card className="w-[300px]">
-          <CardHeader>
-            <CardTitle>Notifications</CardTitle>
-            <CardDescription>You have 3 unread messages.</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <div className="m-2">
-              <ToggleGroupCalendar />
-            </div>
-          </CardContent>
-        </Card>
+        <Header />
+          <Button>Hi</Button>
+
+          <Card className="w-[330px] ml-2">
+            <CardHeader>
+              <CardTitle>Notifications</CardTitle>
+              <CardDescription>You have 3 unread messages.</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="m-2">
+                <ToggleGroupCalendar />
+              </div>
+            </CardContent>
+          </Card>
+
       </main>
-    </div>
   )
 }
