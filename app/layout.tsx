e01a03d53868b8a9362fdf8faeef248a44c7d6ee
Kazykan <rufat@kocherbaev.ru>
2025-01-11 18:48:01 +0300

    add telegram script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,30 @@
-import type { Metadata } from "next"
-import { Jost } from "next/font/google"
-import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
+import type { Metadata } from "next";
+import { Jost } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const jost = Jost({
   subsets: ["cyrillic"],
   variable: "--font-jost",
   weight: ["400", "500", "600", "700", "800", "900"],
-})
+});
 
 export const metadata: Metadata = {
   title: "Motivation v3",
   description: "Motivation for Morgulan Sesenbaev",
-}
+};
 
-export default function RootLayout({
-  children,
-}: React.PropsWithChildren<unknown>) {
+export default function RootLayout({ children }: React.PropsWithChildren<unknown>) {
   return (
     <html lang="en" className="dark" style={{ colorScheme: "dark" }}>
+      <head>
+        <script src="https://telegram.org/js/telegram-web-app.js?56"></script>
+      </head>
       <body className={jost.variable}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
