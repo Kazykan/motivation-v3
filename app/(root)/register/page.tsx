39621a1fcdb323ff04136adac2ed7907b83a095c
Kazykan <rufat@kocherbaev.ru>
2025-01-11 13:48:11 +0300

    add carousel

--- app/(root)/register/page.tsx
+++ app/(root)/register/page.tsx
@@ -1,62 +1,31 @@
+import { CarouselCard } from "@/components/carousel-card";
+import { Button } from "@/components/ui/button";
+import { Rocket } from "lucide-react";
+
 export default function Register() {
+  const images = [
+    "/parent/1 parent.png",
+    "/parent/2 parent.png",
+    "/parent/3 parent.png",
+    "/parent/4 parent.png",
+    "/parent/5 parent.png",
+    "/parent/6 parent.png",
+  ];
+
+  const descriptions = [
+    "Ребенок сам решает, учиться или нет",
+    "Понятные цены и правила, как в реальном мире",
+    "Дети учатся выбирать, как распределить свои усилия",
+    "Оценка работы — объективная, не зависит от настроения",
+    "Система стимулирует дисциплину и активность",
+    "Фокус на результат: Сделал - получил, не сделал - не получил.",
+  ];
+
   return (
-    <main>
-      <header className="bg-blue-500 text-white py-8 px-4">
-        <h1 className="text-3xl font-bold text-center">
-          Уважаемые родители, добро пожаловать в новое измерение воспитания!
-        </h1>
-      </header>
-
-      <section className="container mx-auto p-8">
-        <div className="bg-white p-6 rounded-lg shadow-md">
-          <h2 className="text-2xl font-bold mb-4">
-            Вы стремитесь к тому, чтобы ваш ребенок вырос самостоятельным, ответственным и мотивированным? Тогда вы
-            попали по адресу!
-          </h2>
-          <p className="text-gray-700 mb-4">
-            Наше приложение — это не просто инструмент, это партнер в воспитании, который поможет вам перейти от нотаций
-            и принуждения к взрослым и понятным отношениям с вашим ребенком.
-          </p>
-
-          <h3 className="text-xl font-bold mt-6">Что получает ваш ребенок:</h3>
-          <ul className="list-disc list-inside text-gray-700">
-            <li className="mb-2">
-              <span className="font-bold">✅</span> Получит <b>свободу выбора</b>, а вместе с ней —{" "}
-              <b>ответственность за свои решения</b>.
-            </li>
-            <li className="mb-2">
-              <span className="font-bold">✅</span> Научится самостоятельно управлять своим временем и усилиями.
-            </li>
-            <li className="mb-2">
-              <span className="font-bold">✅</span> Увидит связь между своими действиями и результатом.
-            </li>
-            <li className="mb-2">
-              <span className="font-bold">✅</span> Будет мотивирован <b>достижениями</b>, а не страхом наказания.
-            </li>
-            <li className="mb-2">
-              <span className="font-bold">✅</span> Развиет <b>дисциплину</b>, <b>ответственность</b>,{" "}
-              <b>финансовую грамотность</b>.
-            </li>
-          </ul>
-
-          <h3 className="text-xl font-bold mt-6">Ваша роль:</h3>
-          <p className="text-gray-700 mb-4">
-            Мы создали среду, где нет места эмоциям, есть только четкие правила и “цены”, как в реальной жизни. Вы, как
-            родитель, становитесь архитектором этой среды, устанавливаете правила и наблюдаете за ростом вашего ребенка.
-          </p>
-          <p className="text-gray-700 mb-4">Вы больше не “полицейский”, а наставник и партнер.</p>
-
-          <h3 className="text-xl font-bold mt-6">Важный результат:</h3>
-          <p className="text-gray-700 mb-4">
-            Это ваш шанс воспитать успешного человека, который умеет принимать решения, нести ответственность и
-            добиваться своих целей.
-          </p>
-
-          <p className="text-xl font-bold mt-6 mb-4">
-            <span className="text-blue-600">Начните строить будущее своего ребенка уже сегодня!</span>
-          </p>
-        </div>
-      </section>
+    <main className="flex min-h-screen flex-col items-center p-14">
+      <h1 className="text-lg font-bold mb-8 text-center">Добро пожаловать в новое измерение воспитания!</h1>
+      <CarouselCard images={images} descriptions={descriptions} />
+      <Button><Rocket /> Начать</Button>
     </main>
   );
 }
---
+++ components/carousel-card.tsx
@@ -0,0 +1,63 @@
+"use client";
+import * as React from "react";
+
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+  type CarouselApi,
+} from "@/components/ui/carousel";
+import Image from "next/image";
+
+interface Props {
+  images: string[];
+  descriptions: string[];
+}
+
+export function CarouselCard({ images, descriptions }: Props) {
+  const [api, setApi] = React.useState<CarouselApi>();
+  const [current, setCurrent] = React.useState(0);
+  const [count, setCount] = React.useState(0);
+
+  React.useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
+
+  return (
+    <div className="mx-auto max-w-xs">
+      <Carousel setApi={setApi} className="w-full max-w-xs">
+        <CarouselContent>
+          {Array.from(images).map((image, index) => (
+            <CarouselItem key={index}>
+              <Card>
+                <CardContent className="flex aspect-square items-center justify-center p-0">
+                  <Image className="rounded-2xl" src={image} alt={`Carousel Image ${index + 1}`} width={350} height={350} />
+                </CardContent>
+              </Card>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+      <div className="py-2 text-center text-sm text-muted-foreground">
+        Slide {current} of {count}
+      </div>
+      <div className="py-2 text-center text-sm text-muted-foreground">
+        {descriptions[current - 1]}
+      </div>
+    </div>
+  );
+}
