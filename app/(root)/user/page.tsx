e61a486528b1ee5dd830cc210903d75201996636
Kazykan <rufat@kocherbaev.ru>
2025-01-14 23:39:31 +0300

    delete commit

--- app/(root)/user/page.tsx
+++ app/(root)/user/page.tsx
@@ -16,39 +16,7 @@ const MyComponent: React.FC = () => {
     return <p>Ошибка: Не удалось получить данные пользователя Telegram.</p>;
   }
 
-  return null; // компонент больше ничего не отображает
+  return null;
 };
 
 export default MyComponent;
-
-// 'use client'
-
-// import { TelegramUser } from "@/@types/telegram";
-// import React, { useEffect, useState } from "react";
-
-// const MyComponent: React.FC = () => {
-//   const [telegramUser, setTelegramUser] = useState<TelegramUser | null>(null);
-
-//   useEffect(() => {
-//     if (window.Telegram && window.Telegram.WebApp) {
-//       const webApp = window.Telegram.WebApp;
-//       setTelegramUser(webApp.initDataUnsafe?.user || null);
-//     }
-//   }, []);
-
-//   if (!telegramUser) {
-//     return <p>Loading...</p>;
-//   }
-
-//   return (
-//     <div>
-//       <p>Telegram ID: {telegramUser.id}</p>
-//       <p>Telegram User Name: {telegramUser.username || "N/A"}</p>
-//       <p>First Name: {telegramUser.first_name || "N/A"}</p>
-//       <p>Last Name: {telegramUser.last_name || "N/A"}</p>
-//       {/* ... */}
-//     </div>
-//   );
-// };
-
-// export default MyComponent;
