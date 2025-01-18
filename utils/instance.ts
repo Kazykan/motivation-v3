a11c0da4e88b7d1e8ca7dd1fffc0ec50fd105894
Kazykan <rufat@kocherbaev.ru>
2025-01-18 16:20:05 +0300

    add server instance

--- utils/instance.ts
+++ utils/instance.ts
@@ -1,35 +1,6 @@
 import axios from "axios";
-import { cookies } from "next/headers";
-// import { cookies } from "next/headers";
-// import { redirect } from "next/navigation";
-
-export const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-});
 
 // Создаем экземпляр axios без токена
 export const axiosInstanceWithoutAuth = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-})
-
-// Перехватываем запрос и добавляем токен
-axiosInstance.interceptors.request.use(async (config) => {
-  const token = (await cookies()).get("jwtToken")?.value;
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
 });
-
-// // Добавляем перехватчик ответов для обработки 401 ошибки
-// axiosInstance.interceptors.response.use(
-//   (response) => response, // Просто возвращаем ответ при успехе
-//   async (error) => {
-//     if (error.response && error.response.status === 401) {
-//       // Если ошибка 401, то удаляем токен из куков и перенаправляем на страницу входа
-//       (await cookies()).delete("jwtToken");
-//       redirect("/register");
-//     }
-//     return Promise.reject(error); // Пробрасываем ошибку дальше для других обработок
-//   }
-// );
