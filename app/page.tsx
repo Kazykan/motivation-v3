51c27c118e0a273add44776c81a9d56d86eff04b
Kazykan <rufat@kocherbaev.ru>
2025-01-21 02:17:23 +0300

    delete test card

--- app/page.tsx
+++ app/page.tsx
@@ -8,19 +8,6 @@ export default function Home() {
     <main>
       <Header />
       <PaginationWeeks />
-      <div className="grid gap-4 mt-5">
-        <TestCard task_id={1} frequency={3} taskType={TaskType.DAILY} title="Уборка" description="Уборка комнаты..." sum={34} />
-        <TestCard
-        task_id={2}
-          taskType={TaskType.FLEXIBLE}
-          title="Тренировка"
-          description="You have 3 unread messages."
-          weekdays_need={["Пн", "Ср", "Чт", "Сб", "Вс"]}
-          sum={34}
-        />
-        <TestCard task_id={3} frequency={6} taskType={TaskType.DAILY} title="Duolingo" description="Уборка комнаты..." sum={144} />
-        <TestCard task_id={4} frequency={6} taskType={TaskType.DAILY} title="Уборка" description="Уборка комнаты..." sum={74} />
-      </div>
     </main>
   );
 }
