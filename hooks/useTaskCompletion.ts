3576c01c0ee2705a99dce07a99116653ff34c2c7
Kazykan <rufat@kocherbaev.ru>
2025-01-23 01:10:08 +0300

    optimization

--- hooks/useTaskCompletion.ts
+++ hooks/useTaskCompletion.ts
@@ -6,10 +6,26 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 export const useTaskCompletions = (task_id: string, firstDayOfWeek: Date | null, lastDayOfWeek: Date | null) => {
   return useQuery<TaskCompletionResponse>({
-    queryKey: ["TaskCompletions", task_id, firstDayOfWeek, lastDayOfWeek],
+    queryKey: ["TaskCompletions", task_id, formatDateToYYYYMMDD(firstDayOfWeek!), formatDateToYYYYMMDD(lastDayOfWeek!)],
     queryFn: () => getTaskCompletions(task_id, firstDayOfWeek!, lastDayOfWeek!),
     enabled: !!task_id && !!firstDayOfWeek && !!lastDayOfWeek,
   });
+};
+
+// Функция для получения startDate и endDate
+const useDateRange = () => {
+  const firstDayOfWeek = useWeek((state) => state.start_of_date);
+  const lastDayOfWeek = useWeek((state) => state.end_of_week);
+
+  const startDate = firstDayOfWeek ? formatDateToYYYYMMDD(firstDayOfWeek) : "";
+  const endDate = lastDayOfWeek ? formatDateToYYYYMMDD(lastDayOfWeek) : "";
+
+  return { startDate, endDate };
+};
+
+// Функция для генерации queryKey
+const getTaskCompletionsQueryKey = (taskId: number, startDate: string, endDate: string) => {
+  return ["TaskCompletions", taskId.toString(), startDate, endDate];
 };
 
 interface CreateTaskCompletionVariables {
@@ -23,17 +39,13 @@ interface DeleteTaskCompletionVariables {
 
 export function useCreateTaskCompletion(data: CreateTaskCompletionVariables) {
   const queryClient = useQueryClient();
-  const firstDayOfWeek = useWeek((state) => state.start_of_date);
-  const lastDayOfWeek = useWeek((state) => state.end_of_week);
-
-  const startDate = firstDayOfWeek ? formatDateToYYYYMMDD(firstDayOfWeek) : "";
-  const endDate = lastDayOfWeek ? formatDateToYYYYMMDD(lastDayOfWeek) : "";
+  const { startDate, endDate } = useDateRange();
 
   return useMutation({
     mutationFn: (completionDate: string) => createTaskCompletion(completionDate, data.taskId, data.userId),
     onSuccess: () => {
       queryClient.invalidateQueries({
-        queryKey: ["TaskCompletions", data.taskId.toString(), startDate, endDate],
+        queryKey: getTaskCompletionsQueryKey(data.taskId, startDate, endDate),
       });
     },
   });
@@ -41,17 +53,13 @@ export function useCreateTaskCompletion(data: CreateTaskCompletionVariables) {
 
 export function useDeleteTaskCompletion(data: DeleteTaskCompletionVariables) {
   const queryClient = useQueryClient();
-  const firstDayOfWeek = useWeek((state) => state.start_of_date);
-  const lastDayOfWeek = useWeek((state) => state.end_of_week);
-
-  const startDate = firstDayOfWeek ? formatDateToYYYYMMDD(firstDayOfWeek) : "";
-  const endDate = lastDayOfWeek ? formatDateToYYYYMMDD(lastDayOfWeek) : "";
+  const { startDate, endDate } = useDateRange();
 
   return useMutation({
     mutationFn: (id: number) => deleteTaskCompletion(id),
     onSuccess: () => {
       queryClient.invalidateQueries({
-        queryKey: ["TaskCompletions", data.taskId.toString(), startDate, endDate],
+        queryKey: getTaskCompletionsQueryKey(data.taskId, startDate, endDate),
       });
     },
     onError: (error: any) => {
