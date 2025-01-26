9d4332eacfef562c91f1ca432818bf3ea8561f07
Kazykan <rufat@kocherbaev.ru>
2025-01-26 12:45:25 +0300

    add suspense

--- app/(root)/register/page.tsx
+++ app/(root)/register/page.tsx
@@ -1,47 +1,12 @@
 "use client";
 
-import { getRoleName, payloadRole } from "@/lib/jwt";
-import { useSearchParams } from "next/navigation";
-import React from "react";
+import CheckTelegramUser from "@/components/check-telegram-user";
+import React, { Suspense } from "react";
 
-const CheckTelegramUser: React.FC = () => {
-  const searchParams = useSearchParams();
-  const telegramStartParams = searchParams.get("telegram")
-
-  const [role, setRole] = React.useState<payloadRole | null>(null);
-  const [inviterTelegramId, setInviterTelegramId] = React.useState<number | null>(null);
-  const [showRegistration, setShowRegistration] = React.useState(false);
-
-  React.useEffect(() => {
-    const telegramStartParams = searchParams.get("telegram");
-
-    if (telegramStartParams) {
-      try {
-        const [role, inviterTelegramId] = telegramStartParams.split("_");
-        if (role && inviterTelegramId) {
-          setRole(role === "child" ? payloadRole.child : payloadRole.parent);
-          setInviterTelegramId(Number(inviterTelegramId));
-          setShowRegistration(true);
-        }
-      } catch (error) {
-        console.error("Error parsing telegramStartParams: ", error);
-        setShowRegistration(false);
-      }
-    }
-  }, [searchParams]);
-
+export default function RegisterPage() {
   return (
-    <div>
-      <pre>{telegramStartParams}</pre>
-      {showRegistration ? (
-        <div>
-          Вас пригласил {inviterTelegramId} на роль {getRoleName(role)}
-        </div>
-      ) : (
-        <div>Вас никто не приглашал</div>
-      )}
-    </div>
+    <Suspense fallback={<div>Загрузка...</div>}>
+      <CheckTelegramUser />
+    </Suspense>
   );
-};
-
-export default CheckTelegramUser;
+}
---
+++ components/check-telegram-user.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { getRoleName, payloadRole } from "@/lib/jwt";
+import { useSearchParams } from "next/navigation";
+import React from "react";
+
+const CheckTelegramUser: React.FC = () => {
+  const searchParams = useSearchParams();
+  const telegramStartParams = searchParams.get("telegram");
+
+  const [role, setRole] = React.useState<payloadRole | null>(null);
+  const [inviterTelegramId, setInviterTelegramId] = React.useState<number | null>(null);
+  const [showRegistration, setShowRegistration] = React.useState(false);
+
+  React.useEffect(() => {
+    const telegramStartParams = searchParams.get("telegram");
+
+    if (telegramStartParams) {
+      try {
+        const [role, inviterTelegramId] = telegramStartParams.split("_");
+        if (role && inviterTelegramId) {
+          setRole(role === "child" ? payloadRole.child : payloadRole.parent);
+          setInviterTelegramId(Number(inviterTelegramId));
+          setShowRegistration(true);
+        }
+      } catch (error) {
+        console.error("Error parsing telegramStartParams: ", error);
+        setShowRegistration(false);
+      }
+    }
+  }, [searchParams]);
+
+  return (
+    <div>
+      <pre>{telegramStartParams}</pre>
+      {showRegistration ? (
+        <div>
+          Вас пригласил {inviterTelegramId} на роль {getRoleName(role)}
+        </div>
+      ) : (
+        <div>Вас никто не приглашал</div>
+      )}
+    </div>
+  );
+};
+
+export default CheckTelegramUser;
