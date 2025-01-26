2a59660944748d77777d7a61895bcc8d5f1a50e4
Kazykan <rufat@kocherbaev.ru>
2025-01-26 13:49:29 +0300

    fix link

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,14 @@
+import CheckTelegramUser from "@/components/check-telegram-user";
 import { Header } from "@/components/header";
-import { PaginationWeeks } from "@/components/paginator-week";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main>
       <Header />
-      <PaginationWeeks />
-      <Link href="https://t.me/share/url?text=Привет как дела&url=https://t.me/tukui_bot/app?startapp=172383_child">
-        <Button>Приглашение</Button>
-      </Link>
+      <Suspense fallback={<div>Загрузка...</div>}>
+      <CheckTelegramUser />
+    </Suspense>
     </main>
   );
 }
