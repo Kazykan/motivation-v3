a1852d36a692457350db3f5620b495abf1c677fe
Kazykan <rufat@kocherbaev.ru>
2025-01-26 20:06:16 +0300

    telegramStartParams

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,45 @@
-import CheckTelegramUser from "@/components/check-telegram-user";
-import { Header } from "@/components/header";
-import { Suspense } from "react";
+"use client";
 
-export default function Home() {
+import React from "react";
+import useTelegramUser from "@/hooks/useTelegramUser";
+import CheckTelegramId from "@/components/checkTelegramId";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+const CheckTelegramUser: React.FC = () => {
+  const { telegramUser, telegramStartParams } = useTelegramUser();
+
+  if (!telegramUser) {
+    return <p>Ошибка: Не удалось получить данные пользователя Telegram.</p>;
+  }
+
+
+  
   return (
-    <main>
-      <Header />
-      <Suspense fallback={<div>Загрузка...</div>}>
-      <CheckTelegramUser />
-    </Suspense>
-    </main>
+    <div>
+      <pre>{JSON.stringify(telegramStartParams)}</pre>
+      <Avatar className="h-8 w-8">
+        <AvatarImage src={telegramUser.photo_url} alt="@shadcn" />
+        <AvatarFallback>{telegramUser.first_name?.toUpperCase().slice(0, 2)}</AvatarFallback>
+      </Avatar>
+      {/* <CheckTelegramId telegramStartParams={telegramStartParams} telegramId={telegramUser.id} /> */}
+    </div>
   );
-}
+};
+
+export default CheckTelegramUser;
+
+
+// import CheckTelegramUser from "@/components/check-telegram-user";
+// import { Header } from "@/components/header";
+// import { Suspense } from "react";
+
+// export default function Home() {
+//   return (
+//     <main>
+//       <Header />
+//       <Suspense fallback={<div>Загрузка...</div>}>
+//       <CheckTelegramUser />
+//     </Suspense>
+//     </main>
+//   );
+// }
